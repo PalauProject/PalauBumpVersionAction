9695621d362b10dc42e992db89d787bdea075830
Toby Zethoven <toby@palauproject.com>
2023-08-09 22:27:06 +0200

    add new input value new_version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const fs = require('fs');
 
 function parseVersion(version) {
-    const match = version.match(/^[0-9]+.[0-9]+.[0-9]+$/);
+    const match = version.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
     if (match) {
         version = match[0].split('.');
         return version.map(n => parseInt(n, 10));
@@ -12,20 +12,23 @@ function parseVersion(version) {
 
 async function run() {
     try {
-        let [major, minor, patch] = [0, 0, 0];
-        const bumpPatch = core.getInput("patch") === "true";
-        const bumpMinor = core.getInput("minor") === "true";
-        const bumpMajor = core.getInput("major") === "true";
-    
-        const package = JSON.parse(fs.readFileSync("./package.json", "utf8"));
-        if (package.version) {
-            [major, minor, patch] = parseVersion(package.version);
-        }
+        let newVersion = core.getInput("new_version");
+        if (!parseVersion(newVersion)) {
+            let [major, minor, patch] = [0, 0, 0];
+            const bumpPatch = core.getInput("patch") === "true";
+            const bumpMinor = core.getInput("minor") === "true";
+            const bumpMajor = core.getInput("major") === "true";
         
-        const newPatch = (bumpMajor || bumpMinor) ? 0 : bumpPatch ? patch + 1 : patch;
-        const newMinor = bumpMajor ? 0 : bumpMinor ? minor + 1 : minor;
-        const newMajor = bumpMajor ? major + 1 : major;
-        const newVersion = `${newMajor}.${newMinor}.${newPatch}`;
+            const package = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+            if (package.version) {
+                [major, minor, patch] = parseVersion(package.version);
+            }
+            
+            const newPatch = (bumpMajor || bumpMinor) ? 0 : bumpPatch ? patch + 1 : patch;
+            const newMinor = bumpMajor ? 0 : bumpMinor ? minor + 1 : minor;
+            const newMajor = bumpMajor ? major + 1 : major;
+            newVersion = `${newMajor}.${newMinor}.${newPatch}`;
+        }
 
         const newPackage = { ...package, version: newVersion };
         fs.writeFileSync("./package.json", JSON.stringify(newPackage, null, 2));
